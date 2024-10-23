2760a757eecdbb8e73b5b1160723244c6e50e3da
BrunaAguiarSantos <bruna.santos1218@etec.sp.gov.br>
2024-10-23 12:13:21 -0300

    arrumei o botão

--- SistemaTotem/SistemaTotem/Form1.Designer.cs
+++ SistemaTotem/SistemaTotem/Form1.Designer.cs
@@ -42,7 +42,6 @@
             this.LabelEntrarComo = new System.Windows.Forms.Label();
             this.CheckManterConectado = new System.Windows.Forms.CheckBox();
             this.LoginBtn = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.DesingTelaInicial)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.LogoCPS)).BeginInit();
             this.SuspendLayout();
@@ -50,9 +49,10 @@
             // DesingTelaInicial
             // 
             this.DesingTelaInicial.Image = ((System.Drawing.Image)(resources.GetObject("DesingTelaInicial.Image")));
-            this.DesingTelaInicial.Location = new System.Drawing.Point(-6, -72);
+            this.DesingTelaInicial.Location = new System.Drawing.Point(-8, -89);
+            this.DesingTelaInicial.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.DesingTelaInicial.Name = "DesingTelaInicial";
-            this.DesingTelaInicial.Size = new System.Drawing.Size(512, 668);
+            this.DesingTelaInicial.Size = new System.Drawing.Size(683, 822);
             this.DesingTelaInicial.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.DesingTelaInicial.TabIndex = 5;
             this.DesingTelaInicial.TabStop = false;
@@ -63,9 +63,10 @@
             this.LabelTitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(62)))), ((int)(((byte)(99)))));
             this.LabelTitulo.Font = new System.Drawing.Font("Cambria", 29.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LabelTitulo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.LabelTitulo.Location = new System.Drawing.Point(152, 200);
+            this.LabelTitulo.Location = new System.Drawing.Point(203, 246);
+            this.LabelTitulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LabelTitulo.Name = "LabelTitulo";
-            this.LabelTitulo.Size = new System.Drawing.Size(119, 46);
+            this.LabelTitulo.Size = new System.Drawing.Size(150, 58);
             this.LabelTitulo.TabIndex = 33;
             this.LabelTitulo.Text = "Login";
             this.LabelTitulo.Click += new System.EventHandler(this.LabelTitulo_Click);
@@ -74,9 +75,10 @@
             // 
             this.LogoCPS.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(62)))), ((int)(((byte)(99)))));
             this.LogoCPS.Image = ((System.Drawing.Image)(resources.GetObject("LogoCPS.Image")));
-            this.LogoCPS.Location = new System.Drawing.Point(38, 44);
+            this.LogoCPS.Location = new System.Drawing.Point(51, 54);
+            this.LogoCPS.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.LogoCPS.Name = "LogoCPS";
-            this.LogoCPS.Size = new System.Drawing.Size(100, 80);
+            this.LogoCPS.Size = new System.Drawing.Size(133, 98);
             this.LogoCPS.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.LogoCPS.TabIndex = 34;
             this.LogoCPS.TabStop = false;
@@ -87,9 +89,10 @@
             this.LabelSubTitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(62)))), ((int)(((byte)(99)))));
             this.LabelSubTitulo.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LabelSubTitulo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.LabelSubTitulo.Location = new System.Drawing.Point(111, 265);
+            this.LabelSubTitulo.Location = new System.Drawing.Point(148, 326);
+            this.LabelSubTitulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LabelSubTitulo.Name = "LabelSubTitulo";
-            this.LabelSubTitulo.Size = new System.Drawing.Size(199, 25);
+            this.LabelSubTitulo.Size = new System.Drawing.Size(249, 32);
             this.LabelSubTitulo.TabIndex = 35;
             this.LabelSubTitulo.Text = "Seja Bem Vindo(a)!";
             this.LabelSubTitulo.Click += new System.EventHandler(this.LabelSubTitulo_Click);
@@ -100,9 +103,10 @@
             this.LabelFazerLogin.BackColor = System.Drawing.Color.White;
             this.LabelFazerLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LabelFazerLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(62)))), ((int)(((byte)(99)))));
-            this.LabelFazerLogin.Location = new System.Drawing.Point(718, 113);
+            this.LabelFazerLogin.Location = new System.Drawing.Point(957, 139);
+            this.LabelFazerLogin.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LabelFazerLogin.Name = "LabelFazerLogin";
-            this.LabelFazerLogin.Size = new System.Drawing.Size(200, 25);
+            this.LabelFazerLogin.Size = new System.Drawing.Size(248, 31);
             this.LabelFazerLogin.TabIndex = 36;
             this.LabelFazerLogin.Text = "Faça seu login aqui";
             this.LabelFazerLogin.Click += new System.EventHandler(this.LabelFazerLogin_Click);
@@ -110,18 +114,20 @@
             // TxTUsu
             // 
             this.TxTUsu.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.TxTUsu.Location = new System.Drawing.Point(690, 228);
+            this.TxTUsu.Location = new System.Drawing.Point(920, 281);
+            this.TxTUsu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TxTUsu.Name = "TxTUsu";
-            this.TxTUsu.Size = new System.Drawing.Size(256, 20);
+            this.TxTUsu.Size = new System.Drawing.Size(340, 22);
             this.TxTUsu.TabIndex = 37;
             this.TxTUsu.TextChanged += new System.EventHandler(this.TxTUsu_TextChanged);
             // 
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.textBox1.Location = new System.Drawing.Point(693, 181);
+            this.textBox1.Location = new System.Drawing.Point(924, 223);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(256, 20);
+            this.textBox1.Size = new System.Drawing.Size(340, 22);
             this.textBox1.TabIndex = 38;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -132,9 +138,10 @@
             this.BoxUsu.Items.AddRange(new object[] {
             "Membro Grêmio",
             "Funcionário Secretária"});
-            this.BoxUsu.Location = new System.Drawing.Point(690, 272);
+            this.BoxUsu.Location = new System.Drawing.Point(920, 335);
+            this.BoxUsu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.BoxUsu.Name = "BoxUsu";
-            this.BoxUsu.Size = new System.Drawing.Size(259, 21);
+            this.BoxUsu.Size = new System.Drawing.Size(344, 24);
             this.BoxUsu.TabIndex = 39;
             this.BoxUsu.SelectedIndexChanged += new System.EventHandler(this.BoxUsu_SelectedIndexChanged);
             // 
@@ -142,9 +149,10 @@
             // 
             this.LabelUsuario.AutoSize = true;
             this.LabelUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LabelUsuario.Location = new System.Drawing.Point(602, 181);
+            this.LabelUsuario.Location = new System.Drawing.Point(803, 223);
+            this.LabelUsuario.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LabelUsuario.Name = "LabelUsuario";
-            this.LabelUsuario.Size = new System.Drawing.Size(64, 20);
+            this.LabelUsuario.Size = new System.Drawing.Size(79, 25);
             this.LabelUsuario.TabIndex = 40;
             this.LabelUsuario.Text = "Usuário";
             this.LabelUsuario.Click += new System.EventHandler(this.LabelUsuario_Click);
@@ -153,9 +161,10 @@
             // 
             this.LabelSenha.AutoSize = true;
             this.LabelSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LabelSenha.Location = new System.Drawing.Point(610, 226);
+            this.LabelSenha.Location = new System.Drawing.Point(813, 278);
+            this.LabelSenha.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LabelSenha.Name = "LabelSenha";
-            this.LabelSenha.Size = new System.Drawing.Size(56, 20);
+            this.LabelSenha.Size = new System.Drawing.Size(70, 25);
             this.LabelSenha.TabIndex = 41;
             this.LabelSenha.Text = "Senha";
             this.LabelSenha.Click += new System.EventHandler(this.LabelSenha_Click);
@@ -164,9 +173,10 @@
             // 
             this.LabelEntrarComo.AutoSize = true;
             this.LabelEntrarComo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LabelEntrarComo.Location = new System.Drawing.Point(570, 270);
+            this.LabelEntrarComo.Location = new System.Drawing.Point(760, 332);
+            this.LabelEntrarComo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LabelEntrarComo.Name = "LabelEntrarComo";
-            this.LabelEntrarComo.Size = new System.Drawing.Size(96, 20);
+            this.LabelEntrarComo.Size = new System.Drawing.Size(117, 25);
             this.LabelEntrarComo.TabIndex = 42;
             this.LabelEntrarComo.Text = "Entrar como";
             this.LabelEntrarComo.Click += new System.EventHandler(this.LabelEntrarComo_Click);
@@ -177,11 +187,12 @@
             this.CheckManterConectado.BackColor = System.Drawing.SystemColors.ControlLight;
             this.CheckManterConectado.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CheckManterConectado.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(62)))), ((int)(((byte)(99)))));
-            this.CheckManterConectado.Location = new System.Drawing.Point(723, 346);
+            this.CheckManterConectado.Location = new System.Drawing.Point(972, 395);
+            this.CheckManterConectado.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CheckManterConectado.Name = "CheckManterConectado";
-            this.CheckManterConectado.Size = new System.Drawing.Size(178, 17);
+            this.CheckManterConectado.Size = new System.Drawing.Size(161, 20);
             this.CheckManterConectado.TabIndex = 43;
-            this.CheckManterConectado.Text = "Deseja permanecer conectado?";
+            this.CheckManterConectado.Text = "Não possuí cadastro?";
             this.CheckManterConectado.UseVisualStyleBackColor = false;
             this.CheckManterConectado.CheckedChanged += new System.EventHandler(this.CheckManterConectado_CheckedChanged);
             // 
@@ -189,34 +200,21 @@
             // 
             this.LoginBtn.BackColor = System.Drawing.SystemColors.ControlLight;
             this.LoginBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LoginBtn.Location = new System.Drawing.Point(739, 388);
+            this.LoginBtn.Location = new System.Drawing.Point(982, 437);
+            this.LoginBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.LoginBtn.Name = "LoginBtn";
-            this.LoginBtn.Size = new System.Drawing.Size(150, 41);
+            this.LoginBtn.Size = new System.Drawing.Size(200, 50);
             this.LoginBtn.TabIndex = 44;
             this.LoginBtn.Text = "Login";
             this.LoginBtn.UseVisualStyleBackColor = false;
             this.LoginBtn.Click += new System.EventHandler(this.LoginBtn_Click);
             // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(62)))), ((int)(((byte)(99)))));
-            this.checkBox1.Location = new System.Drawing.Point(723, 314);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(178, 17);
-            this.checkBox1.TabIndex = 45;
-            this.checkBox1.Text = "Deseja permanecer conectado?";
-            this.checkBox1.UseVisualStyleBackColor = false;
-            // 
             // FormLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(1047, 512);
-            this.Controls.Add(this.checkBox1);
+            this.ClientSize = new System.Drawing.Size(1396, 630);
             this.Controls.Add(this.LoginBtn);
             this.Controls.Add(this.CheckManterConectado);
             this.Controls.Add(this.LabelEntrarComo);
@@ -231,6 +229,7 @@
             this.Controls.Add(this.LabelTitulo);
             this.Controls.Add(this.DesingTelaInicial);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormLogin";
             this.Text = "Login";
             this.Load += new System.EventHandler(this.FormLogin_Load);
@@ -256,7 +255,6 @@
         private System.Windows.Forms.Label LabelEntrarComo;
         private System.Windows.Forms.CheckBox CheckManterConectado;
         private System.Windows.Forms.Button LoginBtn;
-        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
